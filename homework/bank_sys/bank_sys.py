5a2f57104a6f4f7e61f0f849fc924ee703f7d478
SesiukalovAA <SesiukalovAA@mpei.ru>
2023-04-06 21:53:31 +0300

    сделал базу для банковсгоко приложения из домашки 1

--- homework/bank_sys/bank_sys.py
+++ homework/bank_sys/bank_sys.py
@@ -16,7 +16,6 @@ def create_account(name: str, balance: int = 0, accounts=BANK):
     if name == '':  # checking client name
         # print('please enter your name')  # may be used for code without try
 
-
         raise ValueError('please enter your name')
     elif balance < 0:  # checking client investment
         # print('incorrect balance')  # may be used for code without try
