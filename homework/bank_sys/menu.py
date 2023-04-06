f76f5120932cc97ac1b4d54735a586793c8fa481
sawergon <SesiukalovAA@mpei.ru>
2023-04-07 00:36:18 +0300

    меню приложения

---
+++ homework/bank_sys/menu.py
@@ -0,0 +1,10 @@
+def print_menu():
+    print('1. create account')
+    print('2. get balance')
+    print('3. put money into account')
+    print('4. withdraw money from account')
+    print('5. transfer money')
+    print('6. withdraw money from account')
+    print('7. exit')
+
+
