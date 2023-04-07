52bc314e0bf53124b4c939bb17d3fb2e9174d740
Nastya <gusak-an@stu.1502.moscow>
2023-04-07 14:31:08 +0300

    Вынесла поиск счёта в отдельную функцию и добавила тесты

--- ch01/Gusak/homework1.py
+++ ch01/Gusak/homework1.py
@@ -1,50 +1,49 @@
+def find_in_accounts(accounts, number):
+    res = list(filter(lambda x: x['number'] == number, accounts))
+    return res[0] if len(res) > 0 else None
+
+
 def create_account(accounts, number, name, amount):
-    for data in accounts:
-        if data['number'] == number:
-            return f'Невозможно открыть счёт с номером счёта {number}'
-    if amount < 0:
+    if find_in_accounts(accounts, number):
+        return f'Невозможно открыть счёт с номером счёта {number}'
+    if amount <= 0:
         return f'Невозможно открыть счёт с начальной суммой {amount}'
     accounts.append({'name': name, 'number': number, 'amount': amount})
     return 'Счёт добавлен'
 
 
 def deposit(accounts, number, amount):
+    data = find_in_accounts(accounts, number)
+    if not data:
+        return f'Счёта {number} не нашлось'
     if amount <= 0:
         return 'Нельзя зачислить неположительную сумму на счёт'
-    for data in accounts:
-        if data['number'] == number:
-            data['amount'] += amount
-            return f'Сумма {amount} успешна зачислена нас счёт {number}'
-    return f'Счёта {number} не нашлось'
+
+    data['amount'] += amount
+    return f'Сумма {amount} успешна зачислена нас счёт {number}'
 
 
 def withdraw(accounts, number, amount):
+    data = find_in_accounts(accounts, number)
+    if not data:
+        return f'Счёта {number} не нашлось'
     if amount <= 0:
         return 'Нельзя снять неположительную сумму с счёта'
-    for data in accounts:
-        if data['number'] == number:
-            if data['amount'] < amount:
-                return 'На счёте недостатоно средств'
-            data['amount'] -= amount
-            return f'Сумма {amount} успешна снята со счёта {number}'
-    return f'Счёта {number} не нашлось'
+    if data['amount'] < amount:
+        return 'На счёте недостатоно средств'
+
+    data['amount'] -= amount
+    return f'Сумма {amount} успешна снята со счёта {number}'
 
 
 def transfer(accounts, sender, receiver, amount):
-    flag1 = False
-    flag2 = False
-    i = 0
-    while (i < len(accounts)) and (not flag1 or not flag2):
-        if accounts[i]['number'] == sender:
-            flag1 = True
-        if accounts[i]['number'] == receiver:
-            flag2 = True
-        i += 1
-    if (not flag1) and (not flag2):
+    data1 = find_in_accounts(accounts, sender)
+    data2 = find_in_accounts(accounts, receiver)
+    if (not data1) and (not data2):
         return f'Счёта {sender} и {receiver} не нашлись'
-    if not flag1:
+    if not data1:
         return f'Счёта {sender} не нашлось'
-    if not flag2:
+    if not data2:
         return f'Счёта {receiver} не нашлось'
     message1 = withdraw(accounts, sender, amount)
     message2 = deposit(accounts, receiver, amount)
@@ -59,12 +58,74 @@ def transfer(accounts, sender, receiver, amount):
 
 
 def get_balance(accounts, number):
-    for data in accounts:
-        if data['number'] == number:
-            return data['amount']
-    return f'Счёта {number} не нашлось'
+    data = find_in_accounts(accounts, number)
+    if not data:
+        return f'Счёта {number} не нашлось'
+
+    return data['amount']
 
 
 bank = [{'name': 'Вася', 'number': 12345, 'amount': 1000},
         {'name': 'Петя', 'number': 67890, 'amount': 1000}]
 
+#  create_account
+#  Такой номер счёта уже есть
+print(create_account(bank, 12345, 'Анастасия', 5000))
+#  Сумма = 0
+print(create_account(bank, 42546, 'Анастасия', 0))
+#  Сумма < 0
+print(create_account(bank, 42546, 'Анастасия', -100))
+#  Можно добавить в банк
+print(create_account(bank, 42546, 'Анастасия', 5000))
+
+print()
+
+#  deposit
+#  Счёта нет
+print(deposit(bank, 163856, 1000))
+#  Сумма = 0
+print(deposit(bank, 42546, 0))
+#  Сумма < 0
+print(deposit(bank, 42546, -100))
+# Можно зачислить
+print(deposit(bank, 42546, 1000))
+
+print()
+
+#  withdraw
+#  Счёта нет
+print(withdraw(bank, 163856, 1000))
+#  Сумма = 0
+print(withdraw(bank, 42546, 0))
+#  Сумма < 0
+print(withdraw(bank, 42546, -100))
+#  Сумма больше той, что на счёте
+print(withdraw(bank, 42546, 10000))
+# Можно снять
+print(withdraw(bank, 42546, 500))
+
+print()
+
+#  transfer
+# Нет счетов sender и receiver
+print(transfer(bank, 275639, 45639, 1000))
+#  Счёта sender нет
+print(transfer(bank, 163856, 42546, 1000))
+#  Счёта receiver нет
+print(transfer(bank, 42546, 163856, 1000))
+#  Сумма = 0
+print(transfer(bank, 42546, 12345, 0))
+#  Сумма < 0
+print(transfer(bank, 42546, 12345, -100))
+#  Сумма больше той, что на счёте
+print(transfer(bank, 42546, 12345, 10000))
+#  Можно перевести
+print(transfer(bank, 42546, 12345, 500))
+
+print()
+
+#  get_balance
+#  Счёта нет
+print(get_balance(bank, 163856))
+# Можно посмотреть баланс
+print(get_balance(bank, 42546))
